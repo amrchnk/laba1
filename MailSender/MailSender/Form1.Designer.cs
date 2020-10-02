2e6ea587081a5197e0dffef12a05347e721052f9
amrchnk <48685431+amrchnk@users.noreply.github.com>
2020-10-02 15:34:48 +0300

    Add files via upload

---
+++ MailSender/MailSender/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace MailSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.to = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MyMail = new System.Windows.Forms.Label();
+            this.text = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Theme = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.OFD = new System.Windows.Forms.OpenFileDialog();
+            this.fileName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // to
+            // 
+            this.to.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.to.Location = new System.Drawing.Point(70, 6);
+            this.to.Name = "to";
+            this.to.Size = new System.Drawing.Size(232, 29);
+            this.to.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Кому";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "От";
+            // 
+            // MyMail
+            // 
+            this.MyMail.AutoSize = true;
+            this.MyMail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MyMail.Location = new System.Drawing.Point(70, 53);
+            this.MyMail.Name = "MyMail";
+            this.MyMail.Size = new System.Drawing.Size(160, 21);
+            this.MyMail.TabIndex = 4;
+            this.MyMail.Text = "nusha21000@mail.ru";
+            // 
+            // text
+            // 
+            this.text.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.text.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.text.Location = new System.Drawing.Point(16, 140);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(575, 191);
+            this.text.TabIndex = 5;
+            this.text.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(16, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Прикрепить файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Theme
+            // 
+            this.Theme.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Theme.Location = new System.Drawing.Point(70, 100);
+            this.Theme.Name = "Theme";
+            this.Theme.Size = new System.Drawing.Size(190, 29);
+            this.Theme.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Тема";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 33);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Отправить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // OFD
+            // 
+            this.OFD.FileName = "openFileDialog1";
+            // 
+            // fileName
+            // 
+            this.fileName.AutoSize = true;
+            this.fileName.Font = new System.Drawing.Font("Segoe UI Symbol", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileName.Location = new System.Drawing.Point(181, 343);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(0, 17);
+            this.fileName.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 424);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Theme);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.MyMail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.to);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox to;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label MyMail;
+        private System.Windows.Forms.RichTextBox text;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Theme;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog OFD;
+        private System.Windows.Forms.Label fileName;
+    }
+}
+
---
+++ MailSender/MailSender/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace MailSender
+{
+    public partial class Form1 : Form
+    {
+        string file = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+      
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fromUser = this.text.Text;    // Получим комментарий пользователя
+            string email = MyMail.Text;          // Получим Email пользователя
+            string password = "yourPassword";
+
+           
+            try
+            {
+                MailAddress from = new MailAddress(email);
+                MailAddress to = new MailAddress(this.to.Text);
+                MailMessage msg = new MailMessage(from, to)
+                {
+                    Subject = Theme.Text,
+                    IsBodyHtml = false,
+                    Body = fromUser,
+
+                };
+                if (file != "") msg.Attachments.Add(new Attachment($"{file}")); ;
+                SmtpClient smtp = new SmtpClient("smtp.mail.ru", 25)
+                {
+                    Credentials = new NetworkCredential(email, password),
+                    EnableSsl = true
+                };
+                smtp.Send(msg);
+                MessageBox.Show("Сообщение успешно отправлено");
+                Close();
+            }
+
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверный формат электронной почты. Почта должна иметь окончания - @gmail/yandex/mail/bk/list и другие");
+                this.to.Clear();
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Строка с адресом не должна быть пуста");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.HelpLink);
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                file = OFD.FileName;
+              this.fileName.Text  = OFD.FileName;
+            }
+        }
+    }
+}
