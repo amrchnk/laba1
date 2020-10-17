6ebb4cc95c497a725f59fde9964af4db541f6cfe
amrchnk <48685431+amrchnk@users.noreply.github.com>
2020-10-17 17:16:25 +0300

    Add files via upload

---
+++ Лабораторная №3/FileExchanger/FileExchanger/FileExchanger/Authorization.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FileExchanger
+{
+    partial class Authorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.lgn = new System.Windows.Forms.TextBox();
+            this.psswrd = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // login
+            // 
+            this.login.AutoSize = true;
+            this.login.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.login.Location = new System.Drawing.Point(268, 181);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(54, 21);
+            this.login.TabIndex = 0;
+            this.login.Text = "Логин";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.password.Location = new System.Drawing.Point(268, 224);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(63, 21);
+            this.password.TabIndex = 1;
+            this.password.Text = "Пароль";
+            // 
+            // lgn
+            // 
+            this.lgn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lgn.Location = new System.Drawing.Point(379, 175);
+            this.lgn.Name = "lgn";
+            this.lgn.Size = new System.Drawing.Size(162, 29);
+            this.lgn.TabIndex = 2;
+            // 
+            // psswrd
+            // 
+            this.psswrd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.psswrd.Location = new System.Drawing.Point(379, 223);
+            this.psswrd.Name = "psswrd";
+            this.psswrd.Size = new System.Drawing.Size(162, 29);
+            this.psswrd.TabIndex = 3;
+            this.psswrd.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(272, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Authorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 528);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.psswrd);
+            this.Controls.Add(this.lgn);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.login);
+            this.Name = "Authorization";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.Authorization_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label login;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox lgn;
+        private System.Windows.Forms.TextBox psswrd;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
